83b08a8ea553d5caab999bd00e99ae52782541e7
vuongpt <vuongpt@bmx.vn>
2016-09-22 09:58:54 +0700

    Initial Commit

---
+++ MusicPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicPlayer
+//
+//  Created by bmxstudio04 on 9/22/16.
+//  Copyright © 2016 bmxstudio04. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MusicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicPlayer
+//
+//  Created by bmxstudio04 on 9/22/16.
+//  Copyright © 2016 bmxstudio04. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
