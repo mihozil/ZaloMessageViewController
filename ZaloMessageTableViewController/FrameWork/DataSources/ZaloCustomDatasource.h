3f002d0906abad5e0683596dbd4f1cab965ba291
Mihozil <minhnht05.sic@gmail.com>
2018-02-06 22:34:16 +0700

    First Commit

---
+++ ZaloMessageTableViewController/FrameWork/DataSources/ZaloCustomDatasource.h
@@ -0,0 +1,13 @@
+//
+//  ZaloCustomDatasource.h
+//  ZaloMessageTableViewController
+//
+//  Created by Mihozil on 2/6/18.
+//  Copyright © 2018 Mihozil. All rights reserved.
+//
+
+#import "ZaloDataSource.h"
+
+@interface ZaloCustomDatasource : ZaloDataSource
+
+@end
---
+++ ZaloMessageTableViewController/FrameWork/DataSources/ZaloCustomDatasource.m
@@ -0,0 +1,13 @@
+//
+//  ZaloCustomDatasource.m
+//  ZaloMessageTableViewController
+//
+//  Created by Mihozil on 2/6/18.
+//  Copyright © 2018 Mihozil. All rights reserved.
+//
+
+#import "ZaloCustomDatasource.h"
+
+@implementation ZaloCustomDatasource
+
+@end
